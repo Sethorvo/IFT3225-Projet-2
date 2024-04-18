2ce0b97aada68080fae4910baf82f04ba2ccb939
Sethorvo <alexandre.eyrolle@hotmail.com>
2024-04-18 16:10:09 -0400

    Correction CSS + Consultation ConceptNet
    Les consultations de conceptNet sont implementes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             const relationContent = $('#relation-template').html();
             const conceptContent = $('#concept-template').html();
 
-            context.$element().html(homeContent);
+            $('#main').html(homeContent);
             $('#help').append(relationContent);
             $('#help').append(conceptContent);
 
@@ -26,13 +26,95 @@ $(document).ready(function() {
                     "dataSrc": ""
                 },
                 "columns": [
-                    { "data": "fact_id" },
                     { "data": "start_concept" },
                     { "data": "label" },
                     { "data": "end_concept" }
                 ]
             });
         });
+
+        this.get('#/concept/:langue/:concept', function(context) {
+            const langue = this.params.langue;
+            const concept = this.params.concept;
+            const template = $('#facts-template').html();
+            $('#main').html(template);
+            $('#facts-table').DataTable({
+                "processing": true,
+                "serverSide": false,
+                "destroy": true,
+                "ajax": {
+                    "url": `https://api.conceptnet.io/query?start=/c/`+ langue + '/' + concept + "&limit=1000",
+                    "type": "GET",
+                    "dataSrc": "edges"
+                },
+                "columns": [
+                    { "data": "start.label" },
+                    { "data": "rel.label" },
+                    { "data": "end.label" }
+                ]
+            });
+        });
+        this.get('#/relation/:relation/from/:langue/:concept', function(context) {
+            const relation = this.params.relation;
+            const langue = this.params.langue;
+            const concept = this.params.concept;
+            const template = $('#facts-template').html();
+            $('#main').html(template);
+            $('#facts-table').DataTable({
+                "processing": true,
+                "serverSide": false,
+                "destroy": true,
+                "ajax": {
+                    "url": `https://api.conceptnet.io/query?start=/c/`+ langue + '/' + concept + "&rel=/r/"+ relation+"&limit=1000",
+                    "type": "GET",
+                    "dataSrc": "edges"
+                },
+                "columns": [
+                    { "data": "start.label" },
+                    { "data": "rel.label" },
+                    { "data": "end.label" }
+                ]
+            });
+        });
+        this.get('#/relation/:relation', function(context) {
+            const relation = this.params.relation;
+            const template = $('#facts-template').html();
+            $('#main').html(template);
+            $('#facts-table').DataTable({
+                "processing": true,
+                "serverSide": false,
+                "destroy": true,
+                "ajax": {
+                    "url": `https://api.conceptnet.io/query?rel=/r/`+ relation + "&limit=1000",
+                    "type": "GET",
+                    "dataSrc": "edges"
+                },
+                "columns": [
+                    { "data": "start.label" },
+                    { "data": "rel.label" },
+                    { "data": "end.label" }
+                ]
+            });
+        });
     });
     app.run('#/help');
+    $('#conc-form').submit(function(event) {
+        event.preventDefault();
+        const langue = $('#conc-lang').val().trim().toLowerCase();
+        const concept = $('#conc-val').val().trim().toLowerCase();
+        window.location.hash = '#/concept/' + encodeURIComponent(langue) + '/' + encodeURIComponent(concept);
+    });
+    $('#rel-form').submit(function(event) {
+        event.preventDefault();
+        const langue = $('#rel-lang').val().trim().toLowerCase();
+        const concept = $('#rel-conc').val().trim().toLowerCase();
+        const relation = $('#rel-val').val().trim();
+        window.location.hash = '#/relation/' + encodeURIComponent(relation)
+            + '/from/' + encodeURIComponent(langue) + "/" + encodeURIComponent(concept);
+    });
+    $('#rel-simple-form').submit(function(event) {
+        event.preventDefault();
+        const relation = $('#rel-simple').val().trim();
+        window.location.hash = '#/relation/' + encodeURIComponent(relation);
+    });
 });
