0ac4c6c9aefe4001d1a7829c0d35da5f8f65f0e1
LucasConan11 <lucas.romain.conan@umontreal.ca>
2024-04-27 18:32:38 -0400

    Refactoring + added CLI arguments support

---
+++ setup/create-db.py
@@ -0,0 +1,130 @@
+import mysql.connector
+from mysql.connector import Error
+import json
+import sqlparse
+import sys
+
+
+# Connexion à MySQL
+def connectToMySQL(host, user, password):
+    try:
+        #Pour que le script fonctionne sur un ordinateur DIRO le nom_usager + "_" doit etre ajouter au debut du nom de BD
+        connection = mysql.connector.connect(host=host, user=user, password=password)
+
+        return connection
+        
+    except Error as e:
+        print(f"Erreur lors de la connexion à MySQL: {e}")
+        return None
+
+
+def loadData(filename):
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+def insertData(connection, facts):
+
+    try:
+
+        cursor = connection.cursor()
+
+        for fact in facts:
+
+            start, relation, end = fact[0], fact[1], fact[2]
+
+            cursor.execute(
+                "INSERT INTO Concepts (label, language) VALUES (%s, 'en') ON DUPLICATE KEY UPDATE concept_id=LAST_INSERT_ID(concept_id)",
+                (start,))
+            start_id = cursor.lastrowid
+
+            # Insérer ou récupérer l'ID pour le concept de fin
+            cursor.execute(
+                "INSERT INTO Concepts (label, language) VALUES (%s, 'en') ON DUPLICATE KEY UPDATE concept_id=LAST_INSERT_ID(concept_id)",
+                (end,))
+            end_id = cursor.lastrowid
+
+            # Insérer ou récupérer l'ID pour la relation
+            cursor.execute(
+                "INSERT INTO Relations (label) VALUES (%s) ON DUPLICATE KEY UPDATE relation_id=LAST_INSERT_ID(relation_id)",
+                (relation,))
+            relation_id = cursor.lastrowid
+
+            # Insérer le fait
+            cursor.execute("INSERT INTO Facts (start_concept_id, relation_id, end_concept_id) VALUES (%s, %s, %s)",
+                            (start_id, relation_id, end_id))
+
+
+        connection.commit()
+        cursor.close()
+        
+    except mysql.connector.Error as e:
+        print(f"Erreur SQL: {e}")
+
+
+def selectDatabase(connection):
+
+        dbName = "conceptnetdb"
+
+        if connection._host == "www-ens":
+            dbName = connection._user + "_" + dbName
+
+        try:
+            cursor = connection.cursor()
+
+            cursor.execute("CREATE DATABASE IF NOT EXISTS " + dbName + ";")
+            connection.commit()
+
+            cursor.execute("USE " + dbName + ";")
+            connection.commit()
+
+        except Error as e:
+            print(f"Erreur lors de la connexion à la database: {e}")
+
+def runSetupQueries(connection, query):
+
+    statements = sqlparse.split(sqlparse.format(query, strip_comments=True))
+
+    try:
+
+        selectDatabase(connection)
+
+        cursor = connection.cursor()
+
+        for statement in statements:
+
+            cursor.execute(statement)
+            connection.commit()
+
+        cursor.close()
+
+    except Error as e:
+        print(f"Erreur lors de l'exécution de la requête: {e}")
+
+
+# Script principal
+def main():
+    host = "localhost" if len(sys.argv) < 2 else sys.argv[1]  # "www-ens" Pour la machine DIRO
+    user = "root" if len(sys.argv) < 3 else sys.argv[2]  # "nom_usager" Pour la machine DIRO
+    password = "" if len(sys.argv) < 4 else sys.argv[3]  # Votre mot de passe MySQL sur la machine DIRO
+
+    connection = connectToMySQL(host, user, password)
+
+    if connection:
+
+        sqlSetupFile = open("setup/init.sql", "r")
+
+        runSetupQueries(connection, sqlSetupFile.read())
+
+        facts = loadData("facts_data.json")
+        insertData(connection, facts)
+
+        sqlSetupFile.close()
+
+        connection.close()
+        
+    else:
+        print("Échec de la connexion à la base de données.")
+
+if __name__ == "__main__":
+    main()
---
+++ setup/init.sql
@@ -0,0 +1,31 @@
+
+SET GLOBAL default_storage_engine = "InnoDB";
+
+-- From https://stackoverflow.com/questions/4922189/drop-multiple-tables-in-one-shot-in-mysql
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS Concepts, Relations, Facts;
+SET foreign_key_checks = 1;
+
+CREATE TABLE Concepts (
+    concept_id INT AUTO_INCREMENT PRIMARY KEY,
+    label VARCHAR(255) NOT NULL,
+    language VARCHAR(2) NOT NULL,
+    UNIQUE (label, language)
+);
+
+CREATE TABLE Relations (
+    relation_id INT AUTO_INCREMENT PRIMARY KEY,
+    label VARCHAR(255) NOT NULL,
+    UNIQUE (label)
+);
+
+CREATE TABLE Facts (
+    fact_id INT AUTO_INCREMENT PRIMARY KEY,
+    start_concept_id INT NOT NULL,
+    relation_id INT NOT NULL,
+    end_concept_id INT NOT NULL,
+
+    FOREIGN KEY (start_concept_id) REFERENCES Concepts(concept_id),
+    FOREIGN KEY (relation_id) REFERENCES Relations(relation_id),
+    FOREIGN KEY (end_concept_id) REFERENCES Concepts(concept_id)
+);
